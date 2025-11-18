c038b3c5ae4e9b5a6a386baa3cce617177897e8e
adzkya-fajri <zelx.arthur@gmail.com>
2025-11-18 17:51:48 +0700

    loading

---
+++ resources/views/loading.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+      :root {
+    --text-head: #f6a40cca;
+    --sur: #832D36
+    }
+    </style>
+</head>
+<body>
+    <main>
+        <section >
+            <div class="box">
+                <h1 class="long" style="color: var(--sur)">cinta dan mikail</h1> 
+                <div class="pos">
+                    
+                </div>
+            </div>
+            
+        </section>
+    </main>  
+</body>
+</html>l  

2f968c25cedad6bc395c6b3f9f518cb35b383847
Ega-telkom <541241041@student.smktelkom-pwt.sch.id>
2025-12-02 22:30:07 +0700

    feat: seed admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'AdminXIPPLG1',                   
+           	'email' => 'adminXIPPLG1@wedding.com',      
+           	'password' => bcrypt('XIPPLG1JAYA'),
+           	'is_admin' => true,   
         ]);
     }
 }
